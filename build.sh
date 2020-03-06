df00b3c5e256dab0d86c0456fd868cdbb5f6864d
awyl <awyl@ylast.com>
2020-03-06 00:58:32 -0500

    update to rc1-12239

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 OC_VERSION=$(cat src/oc/oc.csproj | grep "</OrchardVersion>" | awk -F'OrchardVersion>|</' '{print $2}')
 
 dotnet publish src/oc -c Release --output ./.root/app
-docker build --rm -tawyl/oc:x86-$OC_VERSION .
+docker build --rm -t awyl/oc:x86-$OC_VERSION .
